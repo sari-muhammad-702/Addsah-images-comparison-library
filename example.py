13bbc6bc584b5a48e3cf9c942d1c04ef30ae3066
ساري الزهراني <99333452+sari-muhammad-702@users.noreply.github.com>
2025-09-22 18:16:36 +0300

    Update example.py
    Change image path to suitable with image path ( the file was wrote under the assumption that testing images will be in a privet folder ) .

--- example.py
+++ example.py
@@ -3,12 +3,10 @@ import addsah as ads
 import PIL
 
 # make Image item from PIL.Image class
-img1 = PIL.Image.open('Addsah_test_imgs/مخطط3.png')
-img2 = PIL.Image.open('Addsah_test_imgs/خطين.png')
+img2 = PIL.Image.open('خطين.png')
 
-# perpraring an images to comparison
-ads.perp_img(img1)
+# perpraring an image to comparison - with it self -
 ads.perp_img(img2)
 
 # print the similraty percentage
-print(ads.compa_imgs('Addsah_test_imgs/خطين.png', 'Addsah_test_imgs/خطين.png'))
+print(ads.compa_imgs('خطين.png', 'خطين.png'))
